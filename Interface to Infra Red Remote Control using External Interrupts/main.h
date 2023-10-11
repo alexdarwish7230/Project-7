6db30e7cf1d1ad095c0ae803040a32e70d6457a5
AlexD <106942108+alexdarwish7230@users.noreply.github.com>
2023-10-11 13:42:44 -0700

    Create main.h

---
+++ Interface to Infra Red Remote Control using External Interrupts/main.h
@@ -0,0 +1,16 @@
+#define RD1 PORTEbits.RE0;
+#define GD1 PORTEbits.RE1;
+#define BD1 PORTEbits.RE2;
+
+
+#define RD2 PORTBbits.RB3;
+#define GD2 PORTBbits.RB4;
+#define BD2 PORTBbits.RB5;
+
+#define RD3 PORTDbits.RD4;
+#define GD3 PORTDbits.RD5;
+#define BD3 PORTDbits.RD6;
+
+#define LED_PRESSED PORTCbits.RC0;
+
+
